ec3ff8123a6e6647117cc171de823ce3732dd32b
Leifer Mendez <leifer33@gmail.com>
2020-06-11 14:55:51 +0200

    Fix

--- api/config/parent.db.js
+++ api/config/parent.db.js
@@ -1,59 +1,59 @@
 const mongoose = require('mongoose');
 const PARENT_MONGO = process.env.MONGO_PARENT;
 const moment = require('moment');
-const dbParent = mongoose.createConnection(
-  PARENT_MONGO,
-  {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  (err, conn) => {
-    if (!err) {
-      console.log('*    Connection Parent')
-    }
-  });
-
-
-/**
- * Models
- * @type {Mongoose}
- */
-
-const TenantSchema = new mongoose.Schema(
-  {
-    tenantDomain: {
-      type: String,
-      required: true
-    },
-    status: {
-      type: String,
-      enum: ['active', 'disable', 'blocked'],
-      default: 'active'
-    },
-    methodAllowFree: {
-      type: Array,
-      required: []
-    },
-    features: {
-      type: Array,
-      required: []
-    },
-    startAt: {
-      type: Object,
-      required: true
-    },
-    finishAt: {
-      type: Object,
-      required: true
-    }
-  },
-  {
-    versionKey: false,
-    timestamps: true
-  }
-)
-const Tenant = dbParent.model('Tenant', TenantSchema);
+// const dbParent = mongoose.createConnection(
+//   PARENT_MONGO,
+//   {
+//     keepAlive: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   },
+//   (err, conn) => {
+//     if (!err) {
+//       console.log('*    Connection Parent')
+//     }
+//   });
+//
+//
+// /**
+//  * Models
+//  * @type {Mongoose}
+//  */
+//
+// const TenantSchema = new mongoose.Schema(
+//   {
+//     tenantDomain: {
+//       type: String,
+//       required: true
+//     },
+//     status: {
+//       type: String,
+//       enum: ['active', 'disable', 'blocked'],
+//       default: 'active'
+//     },
+//     methodAllowFree: {
+//       type: Array,
+//       required: []
+//     },
+//     features: {
+//       type: Array,
+//       required: []
+//     },
+//     startAt: {
+//       type: Object,
+//       required: true
+//     },
+//     finishAt: {
+//       type: Object,
+//       required: true
+//     }
+//   },
+//   {
+//     versionKey: false,
+//     timestamps: true
+//   }
+// )
+// const Tenant = dbParent.model('Tenant', TenantSchema);
 
 module.exports = {
 
@@ -70,40 +70,41 @@ module.exports = {
   },
   async checkTenant(req) {
     return new Promise((resolve, reject) => {
-      const tenant = req.clientAccount;
-      Tenant.findOne({
-        tenantDomain: tenant,
-      }, (err, item) => {
-        if (!err) {
-          const finishAt = moment(item.finishAt);
-          const now = moment();
-          const diff = finishAt.diff(now, 'minutes');
-
-          if (!item.status.includes('active')) {
-            reject(null)
-          }
-
-          /**
-           * Limitamos por peticion a usuarios que expiraron
-           */
-          if (diff <= 0) {
-            if (!req.method.includes(item.methodAllowFree)) {
-              reject(null)
-            } else {
-              resolve(
-                {
-                  status: item.status,
-                  minutes: diff
-                }
-              )
-            }
-          }
-
-          resolve(true);
-        } else {
-          reject(null)
-        }
-      })
+      // const tenant = req.clientAccount;
+      // Tenant.findOne({
+      //   tenantDomain: tenant,
+      // }, (err, item) => {
+      //   if (!err) {
+      //     const finishAt = moment(item.finishAt);
+      //     const now = moment();
+      //     const diff = finishAt.diff(now, 'minutes');
+      //
+      //     if (!item.status.includes('active')) {
+      //       reject(null)
+      //     }
+      //
+      //     /**
+      //      * Limitamos por peticion a usuarios que expiraron
+      //      */
+      //     if (diff <= 0) {
+      //       if (!req.method.includes(item.methodAllowFree)) {
+      //         reject(null)
+      //       } else {
+      //         resolve(
+      //           {
+      //             status: item.status,
+      //             minutes: diff
+      //           }
+      //         )
+      //       }
+      //     }
+      //
+      //     resolve(true);
+      //   } else {
+      //     reject(null)
+      //   }
+      // })
+      resolve(true)
     })
   }
 
